d69944010e325901fa14444b4eb24334a84b9a03
Andrei Sumar <azuretester.upon818@passinbox.com>
2026-04-01 00:28:37 +0200

    Adjusted energy behaviour

--- max-devices/tempo-handler/main.js
+++ max-devices/tempo-handler/main.js
@@ -3,16 +3,17 @@ outlets = 5; // 0 = buffer, 1 = energy, 2 = jitter, 3 = st dev, 4 = key root not
 
 var bufferSize = 10;
 
-var energyDropPerSecond = 0.1;
+var energyDropPerSecond = 1;
 
 var jitterSensitivity = 1;
 
-// var cooldownSeconds = 10;
+var topEnergyCooldownSeconds = 30;
 
 var state = {
     buffer: [],
     cooldown: 0,
-    keyRootNote: 0
+    keyRootNote: 0,
+    topEnergyCooldown: 0
 }
 
 var zones = [
@@ -58,7 +59,7 @@ function msg_int(v) {
         energy = normalizeEnergy(energy, v, stdDev);
     }
 
-    outlet(1, v);
+    outlet(1, energy);
 
     outlet(4, state.keyRootNote);
 
@@ -84,19 +85,14 @@ function getRmssd() {
 
 function getStdDev() {
     // Sample standard deviation of the buffer (SDNN-like)
-    var n = state.buffer.length;
-    var mean = 0;
-    for (var i = 0; i < n; i++) {
-        mean += state.buffer[i];
-    }
-    mean /= n;
+    var mean = getMean(0, state.buffer.length);
 
     var sumVar = 0;
-    for (var i = 0; i < n; i++) {
+    for (var i = 0; i < state.buffer.length; i++) {
         var d = state.buffer[i] - mean;
         sumVar += d * d;
     }
-    return Math.sqrt(sumVar / (n - 1));
+    return Math.sqrt(sumVar / (state.buffer.length - 1));
 }
 
 function getTempoZone(tempo) {
@@ -108,33 +104,69 @@ function getTempoZone(tempo) {
     }
 }
 
+function getMean(start, end) {
+    var mean = 0;
+    for (var i = start; i < end; i++) {
+        mean += state.buffer[i];
+    }
+    mean /= (end - start);
+    return mean;
+}
+
 function normalizeEnergy(energy, tempo, stdDev) {
+
+    if (state.topEnergyCooldown > 1) {
+        state.topEnergyCooldown--;
+        return energy;
+    }else if (state.topEnergyCooldown === 1) {
+        state.topEnergyCooldown--;
+        energy = 90;
+    }
+
     tempoZone = getTempoZone(tempo);
 
     var allowedEnergyRange;
+
+    //tod o: hold energy in the last range once it got there
 
     if (tempoZone === 0) {
         allowedEnergyRange = [zones[0][0], zones[1][1]];
     }else if (tempoZone === zones.length - 1) {
         allowedEnergyRange = [zones[tempoZone][0], zones[tempoZone][1]];
     }else{
-        allowedEnergyRange = [zones[tempoZone][0], zones[tempoZone+1][1]];
+        allowedEnergyRange = [zones[tempoZone-1][0], zones[tempoZone+1][1]];
     }
 
     var targetEnergy = energy;
 
-    if (stdDev > 3/* && state.cooldown === 0*/) {
-        targetEnergy += state.buffer[state.buffer.length - 1] - state.buffer[0];
-        state.cooldown = bufferSize;
-    }else{
-        targetEnergy -= energyDropPerSecond;
-    }
 
-    if (targetEnergy < allowedEnergyRange[0]) {
-        targetEnergy = allowedEnergyRange[0];
-    } else if (targetEnergy > allowedEnergyRange[1]) {
-        targetEnergy = allowedEnergyRange[1];
+    var h2mean = getMean(Math.round(state.buffer.length/2), state.buffer.length);
+    var h1mean = getMean(0, Math.round(state.buffer.length/2));
+    var trend = h2mean - h1mean;
+    var trendThreshold = 1.5;
+    var lagQ = Math.max(1, Math.sqrt(Math.abs(tempo-energy)) / 2);
+
+    if ((stdDev < 1.25) && (Math.abs(trend) < trendThreshold)) { //energy falls when the tempo is stable
+        var revLagQ = 1.35/lagQ;
+        targetEnergy -= (1.25 - stdDev) * revLagQ * energyDropPerSecond;
+        if (targetEnergy < allowedEnergyRange[0]) {
+            targetEnergy = allowedEnergyRange[0];
+        }
+    }else if (stdDev > 1.35 && trend > 0) { // rises and is not stable
+        targetEnergy += (stdDev/3.0) * lagQ * Math.max(0, state.buffer[state.buffer.length - 1] - state.buffer[state.buffer.length - 2]);
+        if (targetEnergy > allowedEnergyRange[1]) {
+            targetEnergy = allowedEnergyRange[1];
+        }
+        if (targetEnergy == 177) {
+            state.topEnergyCooldown = topEnergyCooldownSeconds;
+        }
+    }else if (trend < trendThreshold && energy > tempo + 6) { // falls and lags behind tempo       
+        targetEnergy -= (stdDev/3.0) * lagQ * Math.max(0, state.buffer[state.buffer.length - 2] - state.buffer[state.buffer.length - 1]);
+        if (targetEnergy < allowedEnergyRange[0]) {
+            targetEnergy = allowedEnergyRange[0];
+        }
     }
+   
     
     return targetEnergy;
 }
